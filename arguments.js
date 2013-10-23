fc3269479e2762493f43100e38a1487a1b11162f
App Academy Student <student@appacademy.io>
2013-10-23 12:03:46 -0700

    Arguments,currying, and prototypical inheritance exercises

---
+++ arguments.js
@@ -0,0 +1,61 @@
+var sum = function() {
+	var sum = 0
+	for(i=0; i<arguments.length; i++) {
+		sum += arguments[i];
+	}
+	return sum;
+}
+
+// console.log(sum(1,2,3,4));
+
+
+Function.prototype.myBind = function(obj) {
+	var args = Array.prototype.slice.call(arguments);
+	var that = this;
+
+	return function() {
+    var inner_args = Array.prototype.slice.call(arguments);
+		return that.apply(obj, args.concat(inner_args));
+	};
+}
+
+var mySum = sum.myBind(null, 1, 2);
+// console.log(mySum(5));
+
+
+var curriedSum = function(numArgs) {
+	var numbers = [];
+
+	var _curriedSum = function(num){
+		numbers.push(num);
+		if(numbers.length === numArgs){
+			return sum.apply(null, numbers);
+		}
+		else {
+			//console.log(this);
+			return _curriedSum;
+		}
+	};
+	return _curriedSum;
+}
+
+// console.log(curriedSum(4)(1)(2)(3)(4));
+
+Function.prototype.curry = function(numArgs) {
+	var args = [];
+	var that = this;
+
+	var _curriedFunction = function(arg) {
+		args.push(arg);
+		if (args.length === numArgs) {
+			return that.apply(null, args);
+		}
+		else {
+			return _curriedFunction;
+		}
+	};
+
+	return _curriedFunction;
+}
+
+console.log(sum.curry(4)(1)(2)(7)(7));
---
+++ protoInheritance.js
@@ -0,0 +1,31 @@
+Function.prototype.inherits = function(Parent) {
+	function Surrogate() {};
+	Surrogate.prototype = Parent.prototype;
+	this.prototype = new Surrogate();
+}
+
+//testing
+
+function Animal(name) {
+  this.animalName = name;
+};
+
+Animal.prototype.sayHello = function () {
+  console.log("Hello, my name is " + this.animalName);
+};
+
+function Dog(name) {
+	Animal.call(this, name);
+};
+
+Dog.inherits(Animal);
+
+Dog.prototype.bark = function () {
+  console.log("Bark!");
+};
+
+snake = new Animal("Buster");
+snake.sayHello();
+
+sparky = new Dog("Sparky");
+sparky.sayHello();
